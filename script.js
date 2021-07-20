460efdecbe549f3b5b15a0bbeb656e5e8abf57b5
luciano.vtn <luciano.vtn120@gmail.com>
2021-07-20 15:11:12 -0300

    Atualizando projeto en data de 20/07/2021

--- script.js
+++ script.js
@@ -2,10 +2,52 @@ document.body.addEventListener('keyup', (event) => {
     playSound( event.code.toLowerCase() );
 });
 
+/* EVENTS SOUNDS "BUTTON" */
+
+document.querySelector('.composer button').addEventListener('click', () => {
+    let song = document.querySelector('#input').value;
+
+        if (song !== ''){
+            let arraySong = song.split('');
+
+            playComposition(arraySong);
+        }
+});
+
+
+/* KEYBOARD EVENTS "SOUNDS" */
+
 function playSound(sound){
 
     let elementAudio = document.querySelector(`#s_${sound}`);
+    let elementKey   = document.querySelector(`div[data-key="${sound}"]`);
+
         if(elementAudio){
+            elementAudio.currentTime = 0; //CONTROLE DE "DELAY" entre o som e o toque do teclado.
             elementAudio.play();
         }
+
+        if(elementKey){
+            elementKey.classList.add('active');
+
+                setTimeout(()=>{
+                    elementKey.classList.remove('active');
+                }, 280);
+        }
+}
+
+/* "PLAY COMPOSITION FOR BUTTON" */
+
+function playComposition(arraySong) {
+
+    let wait = 0; // CONTROLE do loop 
+
+    for ( let itenSong of arraySong) {
+
+        setTimeout(() => {
+            playSound(`key${itenSong}`);
+        },wait);
+
+        wait += 250; // CONTROLA O TEMPO DE EXECUÇÃO.
+    }
 }
